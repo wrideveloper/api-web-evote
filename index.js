8038f19e657c4a2deb0d37f64cf04a52f5b635f0
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2021-09-30 19:52:41 +0700

    intansiasi vote route pada index

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 // server port
 const port = process.env.PORT || 3000;
 
+// import vote routes
+const voteRoutes = require('./routes/vote.route');
+
+// create vote routes
+app.use('/vote', voteRoutes);
 
 // listen to the port
 app.listen(port, function() {
