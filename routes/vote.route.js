576bd6cb01ed7a84a6aca79861c65ffafcabdadb
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2021-09-30 19:50:55 +0700

    menambahkan route

--- routes/vote.route.js
+++ routes/vote.route.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const voteController = require('../controllers/vote.controller');
+
+// get vote by ID
+router.get('/:id',voteController.getVoteByIDUser);
+
+// get user hasVote
+router.get('/numvote/:id',voteController.getNumVote);
+
+// get sum of vote per candidate by id calon
+router.get('/sumvote/:id', voteController.getSumVoteCandidate);
+
 module.exports = router;
