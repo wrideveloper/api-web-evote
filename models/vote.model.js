4d21068792a4e3e40a57999f5353cd5542768336
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2021-10-01 19:51:19 +0700

    mengubah metode exports model

--- models/vote.model.js
+++ models/vote.model.js
@@ -1,15 +1,7 @@
 let dbCon = require('../config/db.config');
 
-let Vote = function(vote) {
-    this.id_user = vote.id_user;
-    this.id_calon = vote.id_calon;
-    this.harapan = vote.harapan;
-    this.saran = vote.saran;
-    this.waktu_vote = new Date();
-}
-
 // get vote by Id_user
-Vote.getVoteByIDUser = (id, result)=>{
+exports.getVoteByIDUser = (id, result)=>{
     dbCon.query('SELECT * FROM vote WHERE id_user=?', id, (err, res)=>{
         if(err){
             console.log('Error while fetching vote by id user', err);
@@ -21,7 +13,7 @@ Vote.getVoteByIDUser = (id, result)=>{
 }
 
 // get num of vote of user by ID
-Vote.getNumVote = (id, result)=>{
+exports.getNumVote = (id, result)=>{
     dbCon.query('SELECT COUNT(id_vote) as num_vote FROM vote WHERE id_user=?', id, (err, res)=>{
         if(err){
             console.log('Error while fetching num_vote by id user', err);
@@ -33,7 +25,7 @@ Vote.getNumVote = (id, result)=>{
 }
 
 // get sum of vote of candidate from id_calon
-Vote.getSumVoteCandidate = (id, result)=>{
+exports.getSumVoteCandidate = (id, result)=>{
     dbCon.query('SELECT COUNT(id_vote) as sum_vote FROM vote WHERE id_calon=?', id, (err, res)=>{
         if(err){
             console.log('Error while fetching sum_vote by id calon', err);
@@ -43,5 +35,3 @@ Vote.getSumVoteCandidate = (id, result)=>{
         }
     })
 }
-
-module.exports = Vote;
