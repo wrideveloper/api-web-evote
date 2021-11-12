989969cc781b15b03183111bcd243c5b6868a539
Ilham-sinatrio20 <sinatrio20@gmail.com>
2021-11-12 19:47:44 +0700

    Hapus password

--- config/db.config.js
+++ config/db.config.js
@@ -2,15 +2,15 @@ const mysql = require('mysql');
 
 // create mysql connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'ZeroTwo02',
-  database: 'wri_evote'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'wri_evote'
 });
 
 connection.connect(function(error) {
-  if (error) throw error;
-  console.log('Successfully connected to the database.');
+    if (error) throw error;
+    console.log('Successfully connected to the database.');
 })
 
 module.exports = connection;
