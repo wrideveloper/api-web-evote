8537ecb9cb04efe934af0153119a434720ba9ccb
nargyanti <nargyanti@gmail.com>
2021-11-14 21:37:22 +0700

    Add path to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
+const path = require('path');
 
 // Define express
 const app = express();
